c08729e92dbe079bb921745b8cfdba89c31c1e88
PriyanshThakur019 <thakurpriyansh019@gmail.com>
2026-03-23 01:21:31 +0530

    Added cors dependency

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pdfParse = require('pdf-parse');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000']
+  origin: ['http://localhost:3001']
 }));
 
 app.use(express.json());
